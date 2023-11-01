fd0bb881d9c6891f5db7cb0df679fbdf2e3c0961
gilrsantana <g.r.santana@hotmail.com>
2023-11-01 16:22:52 -0300

    forms

--- Front/ProEventos-App/src/app/shared/nav/nav.component.ts
+++ Front/ProEventos-App/src/app/shared/nav/nav.component.ts
@@ -1,20 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
   isCollapsed = true;
 
-  constructor() {
+  constructor(private router: Router) {
     // Empty constructor because we don't need to do anything here
     // If we don't include this, TypeScript will automatically generate an empty constructor for us
     // This is just to make it clear that we don't need to do anything in the constructor
   }
 
-  ngOnInit() {
+  ngOnInit(): void {
     console.log('NavComponent initialized');
   }
+
+  showMenu(): boolean {
+    return this.router.url !== '/user/login';
+  } 
 }
