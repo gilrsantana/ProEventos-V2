19aaeef2514687f5d5abcf7f9105331ae4d618e6
gilrsantana <g.r.santana@hotmail.com>
2023-12-02 22:58:28 -0300

    User Token

---
+++ Back/src/ProEventos.Application/Interfaces/ITokenService.cs
@@ -0,0 +1,8 @@
+using ProEventos.Application.Dtos;
+
+namespace ProEventos.Application.Interfaces;
+
+public interface ITokenService
+{
+    Task<string> CreateToken(UserUpdateDto userUpdateDto);
+}
